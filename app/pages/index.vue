0b1b026b0547d1d40a41d92f66ad0a8bf06981fd
alexanderiksson <Alexerik69@gmail.com>
2025-08-19 15:42:01 +0200

    Setup

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+useSeoMeta({
+    title: "Nuxt Site - Home",
+    description: "Website built using nuxt.js and tailwind css.",
+});
+</script>
+
+<template>
+    <h1>Hello World!</h1>
+</template>
