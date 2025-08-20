4ce1055d637769110197bd2b874ad400909b79c9
alexanderiksson <Alexerik69@gmail.com>
2025-08-20 09:49:57 +0200

    Created intro section etc

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,19 @@
 <template>
     <nav
-        class="fixed left-0 top-0 bottom-0 w-48 flex flex-col justify-between gap-8 py-16 px-8"
+        class="fixed left-0 top-0 bottom-0 w-52 flex flex-col justify-between gap-8 py-16 px-12"
     >
         <NuxtLink to="/" class="text-2xl">Alexander Eriksson</NuxtLink>
 
-        <ul class="flex flex-col gap-2 text-xl font-medium" role="navigation">
+        <ul
+            class="flex flex-col gap-1 text-[22px] font-medium"
+            role="navigation"
+        >
             <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/skills">Skills</NuxtLink></li>
             <li><NuxtLink to="/portfolio">Portfolio</NuxtLink></li>
             <li><NuxtLink to="/contact">Contact</NuxtLink></li>
         </ul>
 
-        <ul class="flex gap-4">
+        <ul class="flex gap-6">
             <li>
                 <a href="">
                     <Icon icon="mdi:github" width="24" height="24" />
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,6 +7,71 @@ useSeoMeta({
 
 <template>
     <div class="content">
-        <h1>Hello World!</h1>
+        <section id="intro" class="flex flex-col gap-8 mb-24">
+            <h1 class="text-4xl font-medium">
+                <div class="flex gap-2 items-center mb-4">
+                    <div>
+                        <img src="/wavinghand.png" width="36px" height="36px" />
+                    </div>
+                    <span class="text-neutral-500">Hello,</span>
+                </div>
+                i'm Alexander Eriksson. Fullstack developer from Stockholm.
+            </h1>
+            <div class="flex gap-2">
+                <NuxtLink class="button">Portfolio</NuxtLink>
+                <NuxtLink class="button">Contact me</NuxtLink>
+            </div>
+        </section>
+
+        <section id="skills" class="bg-slate-300 p-6 rounded-3xl">
+            <h2 class="text-2xl font-medium mb-6">Techstack</h2>
+            <div class="flex justify-between">
+                <div title="HTML">
+                    <Icon icon="flowbite:html-solid" width="24" height="24" />
+                </div>
+                <div title="CSS">
+                    <Icon icon="flowbite:css-solid" width="24" height="24" />
+                </div>
+                <div title="JavaScript">
+                    <Icon
+                        icon="mdi:language-javascript"
+                        width="24"
+                        height="24"
+                    />
+                </div>
+                <div title="React">
+                    <Icon icon="uim:react" width="24" height="24" />
+                </div>
+                <div title="Vue">
+                    <Icon icon="flowbite:vue-solid" width="24" height="24" />
+                </div>
+                <div title="TypeScript">
+                    <Icon
+                        icon="mdi:language-typescript"
+                        width="24"
+                        height="24"
+                    />
+                </div>
+                <div title="Tailwind CSS">
+                    <Icon
+                        icon="flowbite:tailwind-solid"
+                        width="24"
+                        height="24"
+                    />
+                </div>
+                <div title="Wordpress">
+                    <Icon icon="mdi:wordpress" width="24" height="24" />
+                </div>
+                <div title="Node.js">
+                    <Icon icon="grommet-icons:node" width="24" height="24" />
+                </div>
+                <div title="PostgreSQL">
+                    <Icon icon="cib:postgresql" width="24" height="24" />
+                </div>
+                <div title="Figma">
+                    <Icon icon="mage:figma" width="24" height="24" />
+                </div>
+            </div>
+        </section>
     </div>
 </template>
